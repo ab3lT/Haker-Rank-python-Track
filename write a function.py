11a5a52ccfa42745f4249d68b12dc89598ba5d3d
abel <se.abel.tadesse@gmail.com>
2022-10-11 00:23:21 +0300

    function

--- write a function.py
+++ write a function.py
@@ -1,13 +1,9 @@
 def is_leap(year):
     leap = False
-    
-    if year % 4 == 0:
-        leap = True
-    if year % 100 == 0:
-        leep = False
-    if year % 400 == 0:
-        leep = True
-    
+    if year>=1900 and year<=(10**5) :
+        if year % 100 == 0 and year % 400 == 0 or year % 4 == 0:
+            leap = True
+
     return leap
 
 year = int(input())
